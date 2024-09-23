880b6e1aad9fd2fbfcf8c99e478f307a93922476
Rafeeque <rafeequekottakkal046@gmail.com>
2024-09-23 22:10:52 +0530

    deploying on render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000,  // Use dynamic port provided by Render or default to 3000
+    host: '0.0.0.0',  // Bind to all interfaces for external access
+  }
 })
